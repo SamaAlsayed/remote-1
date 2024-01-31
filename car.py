7f13a016541142cd6548afd8734c842faccf518f
SamaAlsayed <sama.alsayed@gmail.com>
2024-01-31 11:50:57 +0300

    random model year feature

--- car.py
+++ car.py
@@ -4,4 +4,12 @@ def getRandomCar():
 	cars = [jeep,porche,subaru]
 	return rabdom.choice(cars)
 
+
+def getRandomyear():
+	
+	years = [2002,1987,2011]
+	return rabdom.choice(years)
+
 print("Randomly selected car:",getRandomCar())
+print("Randomly selected year:",getRandomyear())
+
