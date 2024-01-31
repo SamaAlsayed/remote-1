aee5b9ca3c276fac02eb0a89c17cabd5bcabfe56
SamaAlsayed <sama.alsayed@gmail.com>
2024-01-31 12:23:02 +0300

    initiate commit

--- car.py
+++ car.py
@@ -5,11 +5,8 @@ def getRandomCar():
 	return rabdom.choice(cars)
 
 
-def getRandomyear():
-	
-	years = [2002,1987,2011]
-	return rabdom.choice(years)
+
 
 print("Randomly selected car:",getRandomCar())
-print("Randomly selected year:",getRandomyear())
+
 
